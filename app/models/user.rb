939d56adaa48eb47fe051b860e7e1de408a50867
keita <777keita@14gmail.com>
2019-03-01 17:54:20 +0900

    banana

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+  presence: true,
+  uniqueness: true,
+  format: { with: VALID_EMAIL_REGEX }
+  validates :password,
+  length: { in: 8..32 },
+  format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
+
+
+
+  has_secure_password
 end
